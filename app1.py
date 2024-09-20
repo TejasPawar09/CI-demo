8bf8dee47607a306db8b21b12aa3dbaeff062939
Tejas <tejaspawar1000@gmail.com>
2024-09-20 13:16:27 +0530

    somthing

--- app1.py
+++ app1.py
@@ -10,13 +10,9 @@ def mul(a, b):
     return a * b
 
 
-def sum(a, b):
-    return a + b
+if __name__ == "__main__":
+    result1 = add(4, 5)
+    result2 = sub(4, 1)
+    result3 = mul(5, 5)
 
-
-result1 = add(12, 5)
-result2 = sub(12, 5)
-result3 = mul(12, 50)
-result4 = sum(12, 10)
-
-print(result1, result2, result3)
+    print(result1, result2)
